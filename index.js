25fe59edc9c04aac01682fa2430295a83c07dcc3
adem cirak <adem.cirak@gmail.com>
2019-07-08 14:52:48 +0300

    now can work with service account

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+const {google} = require('googleapis');
 const fs = require("fs");
 const inquirer = require("inquirer");
 const googleAuth = require("./src/google-auth");
@@ -9,94 +9,123 @@ const { parse } = require("./src/caniuse-parser");
 inquirer.registerPrompt("datetime", require("inquirer-datepicker-prompt"));
 
 const outputFilename = "browserslist-stats.json";
+const args = process.argv.slice(2);
 
-googleAuth(oauth2Client => {
-  let selectedProfile;
+const startFlow = oauth2Client => {
+	let selectedProfile;
 
-  getAccounts(oauth2Client)
-    .then((accounts) => {
-      if (accounts.length === 0) {
-        throw new Error('No Google Analytics accounts.')
-      }
+	getAccounts(oauth2Client)
+		.then((accounts) => {
+			if (accounts.length === 0) {
+				throw new Error('No Google Analytics accounts.')
+			}
 
-      return accounts;
-    })
-    .then(accounts =>
-      inquirer.prompt([
-        {
-          type: "list",
-          name: "account",
-          message: "Please select an account:",
-          choices: accounts.map(account => ({
-            value: account,
-            name: `${account.name} (#${account.id})`,
-          })),
-        },
-      ])
-    )
-    .then(({ account }) => getWebProperties(oauth2Client, account.id))
-    .then(webProperties =>
-      inquirer.prompt([
-        {
-          type: "list",
-          name: "webProperty",
-          message: "Please select a property:",
-          choices: webProperties.map(webProperty => ({
-            value: webProperty,
-            name: `${webProperty.name} (#${webProperty.id})`,
-          })),
-        },
-      ])
-    )
-    .then(({ webProperty }) => getProfiles(oauth2Client, webProperty.accountId, webProperty.id))
-    .then(profiles =>
-      inquirer.prompt([
-        {
-          type: "list",
-          name: "profile",
-          message: "Please select a profile:",
-          choices: profiles.map(profile => ({
-            value: profile,
-            name: `${profile.name} (#${profile.id})`,
-          })),
-        },
-      ])
-    )
-    .then(({ profile }) => {
-      const defaultStartDate = new Date();
-      const defaultEndDate = new Date();
+			return accounts;
+		})
+		.then(accounts =>
+			inquirer.prompt([
+				{
+					type: "list",
+					name: "account",
+					message: "Please select an account:",
+					choices: accounts.map(account => ({
+						value: account,
+						name: `${account.name} (#${account.id})`,
+					})),
+				},
+			])
+		)
+		.then(({ account }) => getWebProperties(oauth2Client, account.id))
+		.then(webProperties =>
+			inquirer.prompt([
+				{
+					type: "list",
+					name: "webProperty",
+					message: "Please select a property:",
+					choices: webProperties.map(webProperty => ({
+						value: webProperty,
+						name: `${webProperty.name} (#${webProperty.id})`,
+					})),
+				},
+			])
+		)
+		.then(({ webProperty }) => getProfiles(oauth2Client, webProperty.accountId, webProperty.id))
+		.then(profiles =>
+			inquirer.prompt([
+				{
+					type: "list",
+					name: "profile",
+					message: "Please select a profile:",
+					choices: profiles.map(profile => ({
+						value: profile,
+						name: `${profile.name} (#${profile.id})`,
+					})),
+				},
+			])
+		)
+		.then(({ profile }) => {
+			const defaultStartDate = new Date();
+			const defaultEndDate = new Date();
 
-      selectedProfile = profile;
+			selectedProfile = profile;
 
-      // End date defaults to today, start date defaults to 90 days ago
-      defaultStartDate.setDate(defaultEndDate.getDate() - 90);
+			// End date defaults to today, start date defaults to 90 days ago
+			defaultStartDate.setDate(defaultEndDate.getDate() - 90);
 
-      return inquirer.prompt([
-        {
-          type: "datetime",
-          name: "startDate",
-          message: 'Specify a start date (format is "YYYY-MM-DD", defaults to 90 days ago):',
-          format: ["yyyy", "-", "mm", "-", "dd"],
-          initial: defaultStartDate,
-        },
-        {
-          type: "datetime",
-          name: "endDate",
-          message: 'Specify an end date (format is "YYYY-MM-DD", defaults to today):',
-          format: ["yyyy", "-", "mm", "-", "dd"],
-          initial: defaultEndDate,
-        },
-      ]);
-    })
-    .then(({ startDate, endDate }) => getData(oauth2Client, selectedProfile.id, startDate, endDate))
-    .then(parse)
-    .then(stats => {
-      fs.writeFileSync(outputFilename, JSON.stringify(stats, null, 2));
-      console.log(`Success! Stats saved to '${outputFilename}'`);
-      process.exit();
-    })
-    .catch(err => {
-      console.error(err);
-      process.exit(1);
-    });
-});
+			return inquirer.prompt([
+				{
+					type: "datetime",
+					name: "startDate",
+					message: 'Specify a start date (format is "YYYY-MM-DD", defaults to 90 days ago):',
+					format: ["yyyy", "-", "mm", "-", "dd"],
+					initial: defaultStartDate,
+				},
+				{
+					type: "datetime",
+					name: "endDate",
+					message: 'Specify an end date (format is "YYYY-MM-DD", defaults to today):',
+					format: ["yyyy", "-", "mm", "-", "dd"],
+					initial: defaultEndDate,
+				},
+			]);
+		})
+		.then(({ startDate, endDate }) => getData(oauth2Client, selectedProfile.id, startDate, endDate))
+		.then(parse)
+		.then(stats => {
+			fs.writeFileSync(outputFilename, JSON.stringify(stats, null, 2));
+			console.log(`Success! Stats saved to '${outputFilename}'`);
+			process.exit();
+		})
+		.catch(err => {
+			console.error(err);
+			process.exit(1);
+		});
+}
+
+// we have service account as parameter
+if(args && args.length === 1 && args[0].endsWith('json')) {
+
+	const serviceAccountPath = args[0];
+
+	if(!fs.existsSync(serviceAccountPath)) {
+		throw new Error('Service account file not found! path: ' + serviceAccountPath);
+	}
+
+
+	const serviceAccountJson = JSON.parse(fs.readFileSync(serviceAccountPath));
+
+
+	let scopes = ['https://www.googleapis.com/auth/analytics.readonly'];
+
+	let oauth2Client = new google.auth.JWT(
+		serviceAccountJson.client_email,
+		null,
+		serviceAccountJson.private_key,
+		scopes
+	);
+
+	console.log('client', oauth2Client);
+	startFlow(oauth2Client);
+} else {
+	googleAuth(startFlow);
+}
